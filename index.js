62b59162baaaf79b59998d68b0d542b93ea1fb54
Kevin Mason <krmason2020@gmail.com>
2021-10-30 01:10:47 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = (array) => {
+  if (array.length > 0) {
+    let pop = array.pop();
+    if (Array.isArray(pop)) {
+      return sumItems(pop) + sumItems(array);
+    }
+    return pop + sumItems(array); //accumulates here
+  }
+  return 0; //return the value it starts accumulating with
+};
+
+console.log(sumItems([1, 2, [3, 4]])); //10
 
 module.exports = sumItems;
